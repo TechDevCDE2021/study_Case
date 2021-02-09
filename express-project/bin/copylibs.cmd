806f090f9e56c1eabba119729b8effc4e9a88d50
Clement-Chalencon <clement.chalencon@le-campus-numerique.fr>
2021-02-09 17:19:00 +0100

    templates hbs ok

--- express-project/bin/copylibs.cmd
+++ express-project/bin/copylibs.cmd
@@ -1,4 +1,10 @@
 mkdir public\js\lib
+mkdir public\styles\lib
+
 copy /Y /V node_modules\socket.io\client-dist\socket.io.min.js* public\js\lib\
 copy /Y /V node_modules\@mongodb-js\charts-embed-dom\dist\charts-embed-dom.umd.min.js* public\js\lib\
 copy /Y /V node_modules\axios\dist\axios.min.* public\js\lib\
+copy /Y /V node_modules\bootstrap\dist\js\bootstrap.bundle.min.* public\js\lib\
+copy /Y /V node_modules\jquery\dist\jquery.min.* public\js\lib\
+
+copy /Y /V node_modules\bootstrap\dist\css\bootstrap.min* public\styles\lib\
